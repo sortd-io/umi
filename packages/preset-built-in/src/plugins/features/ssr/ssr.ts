ff9fae22a4dbec4b0c29398948257dad8306fbc7
ycjcl868 <45808948@qq.com>
2021-01-26 16:38:37 +0800

    fix(ssr): built-in `isBrowser` into umi with client

--- packages/preset-built-in/src/plugins/features/ssr/ssr.ts
+++ packages/preset-built-in/src/plugins/features/ssr/ssr.ts
@@ -167,9 +167,7 @@ export default (api: IApi) => {
     );
     api.writeTmpFile({
       path: `${TMP_PLUGIN_DIR}/${CLIENT_EXPORTS}.ts`,
-      content: Mustache.render(clientExportsContent, {
-        SSRUtils: winPath(require.resolve('@umijs/utils/lib/ssr')),
-      }),
+      content: clientExportsContent,
     });
   });
 
